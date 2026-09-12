2bcef77013c5687a5cc8f3ace66897df82c7c083
reamryhodges-cmyk <reamryhodges@gmail.com>
2026-09-13 00:56:50 +0100

    Add admin JavaScript test

---
+++ admin.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+
+  const testButton = document.getElementById("findButton");
+
+  if (testButton) {
+    testButton.addEventListener("click", function () {
+      alert("LocalBoost admin JavaScript is working.");
+    });
+  }
+
+});
